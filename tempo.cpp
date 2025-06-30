4d6047f222e0e5868eea679be4235cfdc0fb0f46
WildSingingCats <angelosatalino224@gmail.com>
2025-06-30 17:19:20 -0400

    tempo

---
+++ tempo.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "headers/utils.h" 
+#include "headers/menu.h"
+#include "headers/oper.h"
+
+
+// esta es un cpp de prueba para probar y luego impremental al main.cpp, para no hacer explotar nada
+// no uso el name space porque puede que nos rompa algo
+// ejemplo de forma:
+// std::cout << ejemplo" << std::endl;
+// igual vs code te corrige casi que todo, asi que no hay problema
+
+int main(){
+
+
+    return 0;
+}
