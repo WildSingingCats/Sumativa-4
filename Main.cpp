8e87d107edc92b9d7082fffc762109536ddd2891
WildSingingCats <angelosatalino224@gmail.com>
2025-06-30 16:54:33 -0400

    Github info

--- Main.cpp
+++ Main.cpp
@@ -13,6 +13,9 @@ using namespace std;
 //  ATT: Angelo Satalino
 //  De momento no he hecho nada, pero aqui estamos jajaja
 //  por favor, documenten el codigo, es importante para que todos podamos entenderlo.
+//  Tambien coloque el projecto en github, por si quieren ver el codigo desde ahi.
+//  https://github.com/WildSingingCats/Sumativa-4
+//  escribanme por WS, dandome su usario de github, para agregarlos al proyecto. :D
 
 int main (){
     pause(); // Call the utility function from utils.h
